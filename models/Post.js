565dcf4b448670a9f6bd2cedf98e38f6eddbff11
jmgerow <justingerow1@gmail.com>
2019-01-13 16:40:50 -0600

    created routes for posts

--- models/Post.js
+++ models/Post.js
@@ -11,5 +11,43 @@ const PostSchema = new Schema({
         type: String, 
         required: true
     },
-    
-})
+    name: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            test: {
+                type: String,
+                required: true
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
